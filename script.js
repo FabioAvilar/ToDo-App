b559e527971e2e5390a3090c3db1c9047b27594f
FabioBassani <avilar.fabio@yahoo.com.br>
2022-08-22 11:38:21 +0200

    im starting toDo app in javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todos = document.getElementById('todos');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const todo = input.value;
+    
+    if(todo) {
+
+    }
+});
